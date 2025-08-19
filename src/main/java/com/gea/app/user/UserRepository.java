88e5506a481e11350a2c3904d71bf63b52a83871
jonathangea <jonathangea09@gmail.com>
2025-08-20 00:47:34 +0700

    Switch authentication from email to username
    Refactored authentication logic to use 'username' instead of 'email' across DTOs, entities, repositories, and service methods. Updated validation, queries, and JWT handling to support username-based authentication and registration. This change improves flexibility and aligns with common authentication practices.

--- src/main/java/com/gea/app/user/UserRepository.java
+++ src/main/java/com/gea/app/user/UserRepository.java
@@ -4,25 +4,26 @@ import com.gea.app.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-    boolean existsByEmail(String email);
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
 
     @Query("""
            select u
            from User u
            left join fetch u.unit un
            left join fetch un.unitType ut
-           where u.email = :email
+           where u.username = :username
            """)
-    Optional<User> findByEmailWithUnit(@Param("email") String email);
-
-    <T> Optional<T> findById(UUID id);
+    Optional<User> findByUsernameWithUnit(@Param("username") String username);
 
     @Query("""
            select u
